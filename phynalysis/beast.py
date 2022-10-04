3c3dd12baa34684ef7b3d516eb9e98c38b54d047
Nicolas Ochsner <nicolas.ochsner@env.ethz.ch>
2022-10-04 10:32:02 +0200

    Add type annotations to beast module

--- phynalysis/beast.py
+++ phynalysis/beast.py
@@ -2,8 +2,11 @@
 
 import pandas as pd
 
+from pathlib import Path
+from typing import Union
 
-def read_beast_log(log_file, burn_in=0):
+
+def read_beast_log(log_file: Union[str, Path], burn_in: Union[int, float] = 0):
     """Read BEAST2 log file and return pandas dataframe."""
     data = pd.read_csv(log_file, sep="\t")
 
