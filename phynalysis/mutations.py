4835347b0191ea2796ee4ac095ce14736623aace
Nicolas Ochsner <nicolas.ochsner@env.ethz.ch>
2022-11-24 15:43:51 +0100

    Add mutations module

---
+++ phynalysis/mutations.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+from typing import Callable, Any
+
+import pandas as pd
+
+from .transform import haplotype_to_list
+
+
+def mutations_from_haplotypes(
+    data: pd.DataFrame,
+    index_map: Callable[[str], Any] = None,
+) -> pd.DataFrame:
+    """Compute mutations from haplotypes."""
+    experiments = defaultdict(lambda: defaultdict(int))
+    for idx, row in data.iterrows():
+        sample_name, haplotype = idx
+        if index_map is not None:
+            sample_name = index_map(sample_name)
+        mutations = haplotype_to_list(haplotype)
+        for mutation in mutations:
+            experiments[sample_name][mutation] += int(row["count"])
+
+    df = pd.DataFrame.from_dict(experiments)
+    df.index.names = ["position", "mutation"]
+    return df
