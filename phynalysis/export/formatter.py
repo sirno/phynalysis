c059135c91a9fd1bfbab2f7fbb111c694c2fea09
Nicolas Ochsner <nicolas.ochsner@env.ethz.ch>
2023-02-23 17:01:29 +0100

    Allow formatter to handle missing args

--- phynalysis/export/formatter.py
+++ phynalysis/export/formatter.py
@@ -12,5 +12,8 @@ class IncrementalFormatter(string.Formatter):
     def get_value(self, key, args, kwds):
         if isinstance(key, str):
             return kwds.get(key, self.default.format(key))
-        else:
-            return string.Formatter.get_value(key, args, kwds)
+
+        if isinstance(key, int) and len(args) > key:
+            return args[key]
+
+        return self.default.format("")
