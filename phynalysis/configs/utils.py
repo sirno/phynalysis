24200c7573df34ce1d3ea70af572413a0a84fd36
Nicolas Ochsner <nicolas.ochsner@env.ethz.ch>
2023-04-05 15:01:50 +0200

    Revise expansion code

--- phynalysis/configs/utils.py
+++ phynalysis/configs/utils.py
@@ -5,30 +5,25 @@ import re
 def _expand_path(path: str) -> list[str]:
     paths = []
     expansion_stack = [path]
-    range_regex = re.compile(r"\[(?P<start>\d+)-(?P<end>\d+)\]")
     list_regex = re.compile("\[(.*?)\]")
     while expansion_stack:
         path = expansion_stack.pop(0)
 
-        range_match = re.search(range_regex, path)
-
-        if range_match:
-            for item in range(
-                int(range_match.group("start")),
-                int(range_match.group("end")) + 1,
-            ):
-                expanded_path = re.sub(range_regex, str(item), path, count=1)
-                expansion_stack.append(expanded_path)
-            continue
-
         list_match = re.search(list_regex, path)
 
-        if list_match:
-            for item in list_match.group(1).split(","):
-                expanded_path = re.sub(list_regex, item, path, count=1)
-                expansion_stack.append(expanded_path)
+        if list_match is None:
+            paths.append(path)
             continue
 
-        paths.append(path)
+        inner = list_match.group(1)
+
+        if ".." in inner:
+            items = range(*map(int, inner.split("..")))
+        else:
+            items = inner.split(",")
+
+        for item in items:
+            expanded_path = re.sub(list_regex, item, path, count=1)
+            expansion_stack.append(expanded_path)
 
     return sorted(paths)
