a721bed66233a3f3dba940d43042f8fdfc8a6dcf
Nicolas Ochsner <nicolas.ochsner@env.ethz.ch>
2023-04-19 12:55:05 +0200

    Changes to remote client

--- phynalysis/remote/client.py
+++ phynalysis/remote/client.py
@@ -3,6 +3,7 @@
 import paramiko
 import errno
 import rich
+import scp
 import stat
 
 
@@ -16,6 +17,7 @@ class PhynalysisRemoteClient:
 
         self._ssh_client = None
         self._sftp_client = None
+        self._scp_client = None
 
         self._sftp_client = None
 
@@ -37,7 +39,15 @@ class PhynalysisRemoteClient:
 
         return self._sftp_client
 
-    def exists(self, path):
+    @property
+    def scp_client(self):
+        """SCP client."""
+        if self._scp_client is None:
+            self._scp_client = scp.SCPClient(self.ssh_client.get_transport())
+
+        return self._scp_client
+
+    def exists(self, path: str):
         """Check if a path exists."""
         try:
             self.sftp_client.stat(path)
@@ -48,31 +58,40 @@ class PhynalysisRemoteClient:
         else:
             return True
 
-    def is_dir(self, path):
+    def is_dir(self, path: str):
         """Check if a path is a directory."""
         return stat.S_ISDIR(self.sftp_client.stat(path).st_mode)
 
-    def is_file(self, path):
+    def is_file(self, path: str):
         """Check if a path is a file."""
         return stat.S_ISREG(self.sftp_client.stat(path).st_mode)
 
-    def mkdir(self, path):
+    def mkdir(self, path: str):
         """Make directory."""
         self.execute(f"mkdir -p {path}", print_output=False)
 
-    def listdir(self, path):
+    def mkdirs(self, paths: list[str]):
+        """Make directories."""
+        self.execute(f"mkdir -p {' '.join(paths)}", print_output=False)
+
+    def listdir(self, path: str):
         """List directory."""
         return self.sftp_client.listdir(path)
 
-    def get(self, remote_path, local_path):
+    def get(self, remote_path: str, local_path: str):
         """Get a file."""
-        self.sftp_client.get(remote_path, local_path)
+        self.scp_client.get(remote_path, local_path)
 
-    def put(self, local_path, remote_path):
+    def put(self, local_path: str, remote_path: str):
         """Put a file."""
-        self.sftp_client.put(local_path, remote_path)
+        self.scp_client.put(local_path, remote_path)
 
-    def execute(self, command, print_output=True, print_error=True):
+    def execute(
+        self,
+        command: str,
+        print_output: bool = True,
+        print_error: bool = True,
+    ):
         """Execute a command."""
         stdin, stdout, stderr = self.ssh_client.exec_command(command)
 
@@ -90,7 +109,7 @@ class PhynalysisRemoteClient:
 
         return stdout.channel.recv_exit_status()
 
-    def find(self, path, pattern):
+    def find(self, path: str, pattern: str):
         """Find files."""
         stdin, stdout, stderr = self.ssh_client.exec_command(f"fd . {path} {pattern}")
         return stdout.read().decode("utf-8").strip().split("\n")
