aee9e20c8db7e282b070e3db2069a4f24dff74c1
Nicolas Ochsner <nicolas.ochsner@env.ethz.ch>
2023-06-02 10:57:55 +0200

    Test aggregate function

---
+++ tests/test_cli.py
@@ -0,0 +1,25 @@
+"""Test cli functions."""
+
+import argparse
+import io
+
+import pandas as pd
+
+from phynalysis.cli import aggregate
+
+
+def test_aggregate():
+    output_buffer = io.StringIO()
+    args = argparse.Namespace(
+        barcodes="tests/data/barcodes.csv",
+        input=[
+            "tests/data/sample_200_1.haplotypes.csv",
+            "tests/data/sample_200_2.haplotypes.csv",
+        ],
+        output=output_buffer,
+    )
+    aggregate.aggregate(args)
+    output_buffer.seek(0)
+    output = pd.read_csv(output_buffer)
+    expected_output = pd.read_csv("tests/data/samples.haplotypes.csv")
+    pd.testing.assert_frame_equal(output, expected_output)
