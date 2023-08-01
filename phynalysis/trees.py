5de90d9b5304654fdbe93bd79d40ca7255741060
Nicolas Ochsner <ochsnern@student.ethz.ch>
2023-08-01 13:35:52 +0200

    Add trees module which wraps ete4

---
+++ phynalysis/trees.py
@@ -0,0 +1,25 @@
+"""Helper functions to read, write and process trees for phynalysis."""
+
+from pathlib import Path
+from typing import Union
+
+import ete4
+
+def read_tree(path: Union[str, Path]) -> ete4.Tree:
+    """Read tree from file."""
+    newick_tree = Path(path).read_text()
+    return ete4.Tree(newick_tree)
+
+
+def prune_tree(tree: ete4.Tree, taxa: list) -> ete4.Tree:
+    """Prune tree and return strictly bifurcating tree."""
+    tree = tree.copy()
+    tree.prune(taxa)
+    tree.resolve_polytomy(recursive=True)
+    return tree
+
+
+def write_tree(tree: ete4.Tree, path: Union[str, Path]):
+    """Write tree to file."""
+    newick_tree = tree.write()
+    Path(path).write_text(newick_tree)
