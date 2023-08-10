0e8be6a429462cb5b23d03f0305873c733ec4b57
Nicolas Ochsner <nicolas.ochsner@env.ethz.ch>
2023-08-10 15:20:35 +0200

    Drop duplicates in barcode list

--- phynalysis/cli/aggregate.py
+++ phynalysis/cli/aggregate.py
@@ -9,7 +9,7 @@ def aggregate(args):
     """Aggregate command main function."""
 
     # load files
-    barcodes = pd.read_csv(args.barcodes)
+    barcodes = pd.read_csv(args.barcodes).drop_duplicates()
     samples = [pd.read_csv(file) for file in args.input]
 
     # extract barcodes from file names
@@ -22,9 +22,7 @@ def aggregate(args):
     haplotypes.reset_index(inplace=True)
 
     # merge barcodes
-    haplotypes_annotated = haplotypes.merge(
-        barcodes, on="barcode", how="left"
-    ).reset_index()
+    haplotypes_annotated = pd.merge(haplotypes, barcodes, on="barcode")
 
     # write output
     haplotypes_annotated.to_csv(args.output, index=False)
